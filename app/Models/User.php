181127b3230a5fbdd25037562a4743bf81072329
Abdulloh <akramovabdullo2002@gmail.com>
2023-01-24 17:08:20 +0300

    Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function name() :Attribute
+    {
+        return new Attribute(
+            get: fn($value) => strtoupper($value),
+            set: fn($value) =>$value
+        );
+    }
 }
