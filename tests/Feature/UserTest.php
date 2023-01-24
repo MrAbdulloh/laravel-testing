eb50453b2414a2bd46b81ce9e0b8984cefd0ef16
Abdulloh <akramovabdullo2002@gmail.com>
2023-01-24 17:08:29 +0300

    test

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -9,5 +9,48 @@ use Tests\TestCase;
 
 class UserTest extends TestCase
 {
+    use RefreshDatabase;
 
+    /** @test */
+    public function login_redirect_to_dashboard_successfully()
+    {
+        User::factory()->create([
+            'email' => 'test@test.com',
+            'password' => bcrypt('password')
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => 'test@test.com',
+            'password' => 'password'
+        ]);
+        $response->assertStatus(302);
+        $response->assertRedirect('/dashboard');
+    }
+
+    /** @test */
+    public function auth_user_can_access_dashboard()
+    {
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->get('/dashboard');
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function auth_user_cannot_access_dasboard()
+    {
+        $response = $this->get('dashboard');
+        $response->assertStatus(302);
+        $response->assertRedirect('login');
+    }
+
+    /** @test */
+    public function user_has_name_attribute()
+    {
+        $user = User::factory()->create([
+            'name' => 'John',
+            'email' => 'test@test.com',
+            'password' => bcrypt('password')
+        ]);
+        $this->assertEquals(strtoupper('John'), $user->name);
+    }
 }
